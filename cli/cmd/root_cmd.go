766cae4c309899638f3beeea955c89ffb289cf7b
Hedzr Yeh <hedzrz@gmail.com>
2019-10-05 17:27:25 +0800

    refine the go files

--- cli/cmd/root_cmd.go
+++ cli/cmd/root_cmd.go
@@ -444,46 +444,6 @@ func attachConsulConnectFlags(cmd cmdr.OptCmd) {
 
 }
 
-func kvBackup(cmd *cmdr.Command, args []string) (err error) {
-	// err = consul.Backup()
-	return
-}
-
-func kvRestore(cmd *cmdr.Command, args []string) (err error) {
-	// err = consul.Restore()
-	return
-}
-
-func msList(cmd *cmdr.Command, args []string) (err error) {
-	// err = consul.ServiceList()
-	return
-}
-
-func msTagsList(cmd *cmdr.Command, args []string) (err error) {
-	// err = consul.TagsList()
-	return
-}
-
-func msTagsAdd(cmd *cmdr.Command, args []string) (err error) {
-	// err = consul.Tags()
-	return
-}
-
-func msTagsRemove(cmd *cmdr.Command, args []string) (err error) {
-	// err = consul.Tags()
-	return
-}
-
-func msTagsModify(cmd *cmdr.Command, args []string) (err error) {
-	// err = consul.Tags()
-	return
-}
-
-func msTagsToggle(cmd *cmdr.Command, args []string) (err error) {
-	// err = consul.TagsToggle()
-	return
-}
-
 const (
 	appName   = "cmdr-http2"
 	copyright = "cmdr-http2 is an demo app"
