963f7b83b64c77928942a11ffeff456d89c2224b
Hedzr Yeh <hedzrz@gmail.com>
2020-01-02 15:57:24 +0800

    better coding idioms
    Signed-off-by: Hedzr Yeh <hedzrz@gmail.com>

--- internal/sample/cmd.go
+++ internal/sample/cmd.go
@@ -10,13 +10,18 @@ func WithSampleCmdrOption() cmdr.ExecOption {
 
 		w.AddOnBeforeXrefBuilding(func(root *cmdr.RootCommand, args []string) {
 			// the following statements show you how to attach an option to a sub-command
-			serverCmd := cmdr.FindSubCommandRecursive("server", nil)
-			serverStartCmd := cmdr.FindSubCommand("start", serverCmd)
-			cmdr.NewInt(5100).
-				Titles("vnc", "vnc-server").
-				Description("start as a vnc server (just a demo)", "").
-				Placeholder("PORT").
-				AttachTo(cmdr.NewCmdFrom(serverStartCmd))
+			var serverCmd, serverStartCmd *cmdr.Command
+			serverCmd = cmdr.FindSubCommandRecursive("server", nil)
+			if serverCmd != nil {
+				serverStartCmd = cmdr.FindSubCommand("start", serverCmd)
+			}
+			if serverStartCmd != nil {
+				cmdr.NewInt(5100).
+					Titles("vnc", "vnc-server").
+					Description("start as a vnc server (just a demo)", "").
+					Placeholder("PORT").
+					AttachTo(cmdr.NewCmdFrom(serverStartCmd))
+			}
 		})
 	}
 }
